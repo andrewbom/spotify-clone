884454793c72a837764b2d5f550ecc5c041df379
andrewbom <59633536+andrewbom@users.noreply.github.com>
2020-08-19 21:30:28 +1000

    completed sidebar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import SpotifyWebApi from "spotify-web-api-js";
+import { useDataLayerValue } from "./DataLayer";
+import Player from "./Player";
+import { getTokenFromResponse } from "./spotify";
+import "./App.css";
+import Login from "./Login";
+
+const spotify = new SpotifyWebApi();
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [{ user, token }, dispatch] = useDataLayerValue();
+
+  useEffect(() => {
+    // Set token
+    const hash = getTokenFromResponse();
+    window.location.hash = "";
+    let _token = hash.access_token;
+
+    if (_token) {
+      dispatch({
+        type: "SET_TOKEN",
+        token: _token,
+      });
+
+      spotify.setAccessToken(_token);
+      spotify.getMe().then((user) => {
+        dispatch({
+          type: "SET_USER",
+          user: user,
+        });
+      });
+
+      spotify.getUserPlaylists().then((playlists) => {
+        dispatch({
+          type: "SET_PLAYLISTS",
+          playlists: playlists,
+        });
+      });
+    }
+
+    //   s.getPlaylist("37i9dQZEVXcJZyENOWUFo7").then((response) =>
+    //     dispatch({
+    //       type: "SET_DISCOVER_WEEKLY",
+    //       discover_weekly: response,
+    //     })
+    //   );
+
+    //   s.getMyTopArtists().then((response) =>
+    //     dispatch({
+    //       type: "SET_TOP_ARTISTS",
+    //       top_artists: response,
+    //     })
+    //   );
+
+    //   dispatch({
+    //     type: "SET_SPOTIFY",
+    //     spotify: s,
+    //   });
+
+    //   s.getMe().then((user) => {
+    //     dispatch({
+    //       type: "SET_USER",
+    //       user,
+    //     });
+    //   });
+  }, []); //token, dispatch
+
+  return <div className="app">{token ? <Player spotify={spotify} /> : <Login />}</div>;
 }
 
 export default App;
---
+++ src/reducer.js
@@ -0,0 +1,71 @@
+import { findAllByDisplayValue } from "@testing-library/react";
+
+export const initialState = {
+  user: null,
+  playlists: [],
+  spotify: null,
+  discover_weekly: null,
+  top_artists: null,
+  playing: false,
+  item: null,
+  // remove after finished developing
+  token:
+    "BQBCKp6UmmqRnIWvWLlKrRqwr-RaHcGR0OWLGP5Rd05n0Lzux-ZFfPiXffIGZ4hYthU12CwynnBY65x7YiCOmUHcHtfCNElwIcHCeeDIDg3bo4E0uCH7-NvE2fohGgKN9zfm_cQ-E2GGYe2jbhhtSBqxzF9MVbgQsA",
+};
+
+const reducer = (state, action) => {
+  console.log(action);
+  switch (action.type) {
+    case "SET_USER":
+      return {
+        ...state,
+        user: action.user,
+      };
+
+    case "SET_PLAYING":
+      return {
+        ...state,
+        playing: action.playing,
+      };
+
+    case "SET_ITEM":
+      return {
+        ...state,
+        item: action.item,
+      };
+
+    case "SET_DISCOVER_WEEKLY":
+      return {
+        ...state,
+        discover_weekly: action.discover_weekly,
+      };
+
+    case "SET_TOP_ARTISTS":
+      return {
+        ...state,
+        top_artists: action.top_artists,
+      };
+
+    case "SET_TOKEN":
+      return {
+        ...state,
+        token: action.token,
+      };
+
+    case "SET_SPOTIFY":
+      return {
+        ...state,
+        spotify: action.spotify,
+      };
+
+    case "SET_PLAYLISTS":
+      return {
+        ...state,
+        playlists: action.playlists,
+      };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
