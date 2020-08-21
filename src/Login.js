59bfd7165ac25ba4ff5b30c8578189980ad84157
andrewbom <59633536+andrewbom@users.noreply.github.com>
2020-08-21 17:48:30 +1000

    update

--- src/Login.js
+++ src/Login.js
@@ -11,6 +11,11 @@ const Login = () => {
       />
 
       <a href={accessUrl}>LOGIN TO SPOTIFY</a>
+
+      <p>
+        Please note that this is the spotify-clone for remoting actual spotify app purpose only. If
+        you wish to play the music, please open your own spotify app BEFORE you login.
+      </p>
     </div>
   );
 };
